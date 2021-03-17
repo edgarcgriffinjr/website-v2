9d6bfdaa21ae3f4b0a95bb72c563a98071ac45d3
Edgar Griffin <edgarcgriffinjr@gmail.com>
2021-03-16 21:17:24 -0400

    update resume

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { HashLink as Link} from 'react-router-hash-link'
+import { HashLink as Link } from 'react-router-hash-link'
 import * as AiIcons from 'react-icons/ai'
 import * as CgIcons from 'react-icons/cg'
 
-import pdf from '../assets/docs/resume.pdf'
+import pdf from '../assets/docs/edgargriffin-software-engineer.pdf'
 import { navData } from '../static/navData'
 import '../assets/css/nav.css'
-import Logo  from '../static/logo'
+import Logo from '../static/logo'
 
 
 const Nav = () => {
@@ -14,69 +14,69 @@ const Nav = () => {
 
     const showSidebar = () => setSidebar(!sidebar)
 
-    return ( 
-        <div className = 'nav'>
+    return (
+        <div className='nav'>
             <div className="header">
                 <div className="header-box">
                     <div className="logo">
-                        <Link smooth to = '#'> <Logo /></Link>
+                        <Link smooth to='#'> <Logo /></Link>
                     </div>
 
                     <nav className="link-list-item">
                         {navData.map((item, index) => {
                             return (
-                                <li key = {index} className = {item.cName}>
-                                    <Link smooth to = {item.path}>
+                                <li key={index} className={item.cName}>
+                                    <Link smooth to={item.path}>
                                         {item.icon}
                                         <span> {item.title} </span>
                                     </Link>
                                 </li>
                             )
                         })}
-                        <a className = 'nav-button' href = {pdf} target = '_blank' rel = 'noreferrer'>
+                        <a className='nav-button' href={pdf} target='_blank' rel='noreferrer'>
                             <button> Resume </button>
                         </a>
                     </nav>
                 </div>
             </div>
-            
-            <div className = "navbar">
-                <div className = "navbar-logo">
-                    <Link smooth to = '#'> <Logo /></Link>
+
+            <div className="navbar">
+                <div className="navbar-logo">
+                    <Link smooth to='#'> <Logo /></Link>
                 </div>
 
-                <div className = 'navbar-burger'>
-                    <div className = 'burger'>
-                        <CgIcons.CgMenuRight onClick = {showSidebar} />
+                <div className='navbar-burger'>
+                    <div className='burger'>
+                        <CgIcons.CgMenuRight onClick={showSidebar} />
                     </div>
                 </div>
             </div>
 
-            <nav className= {sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className="nav-menu-item" onClick = {showSidebar}>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className="nav-menu-item" onClick={showSidebar}>
                     <li className="navbar-toggle">
-                        <Link smooth to ='#' className = 'close'>
+                        <Link smooth to='#' className='close'>
                             <AiIcons.AiOutlineClose />
                         </Link>
                     </li>
                     {navData.map((item, index) => {
                         return (
-                            <li key = {index} className = {item.cName}>
-                                <Link smooth to = {item.path}>
+                            <li key={index} className={item.cName}>
+                                <Link smooth to={item.path}>
                                     {item.icon}
                                     <span> {item.title} </span>
                                 </Link>
                             </li>
                         )
                     })}
-                    <a className = 'nav-button' href = {pdf} target = '_blank' rel = 'noreferrer'>
-                            <button> Resume </button>
+                    <a className='nav-button' href={pdf} target='_blank' rel='noreferrer'>
+                        <button> Resume </button>
                     </a>
 
                 </ul>
-            </nav>  
+            </nav>
         </div>
     );
 }
- 
+
 export default Nav;
