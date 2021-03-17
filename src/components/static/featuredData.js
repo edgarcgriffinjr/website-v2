5ca68caa242503585ad20edddc4bd02849cce237
Edgar Griffin <edgarcgriffinjr@gmail.com>
2021-03-16 21:13:57 -0400

    updating project component and adding more images/formatting

--- src/components/static/featuredData.js
+++ src/components/static/featuredData.js
@@ -1,5 +1,6 @@
-import web from '../assets/images/Website-v2.jpeg'
+import web from '../assets/images/website-v2.png'
 import cnn from '../assets/images/cnn.png'
+import maker from '../assets/images/maker.png'
 
 
 export const featuredData = [
@@ -7,14 +8,24 @@ export const featuredData = [
     id: 1,
     title: "Portfolio Website",
     github: "https://github.com/edgarcgriffinjr/website-v2",
-    url: 'null',
-    desc: "This is my first attempt at a portfoilio website to break into the Software Engineering world."+
-            "This website was created with Gatsby which is mostly based on React.js.",
-    tech: ['Javascript', 'CSS', 'SCSS', 'HTML', 'React.js', 'GatsbyJS'], 
+    url: 'https://edgargriffin.com',
+    desc: "Website built with React that is styled with CSS using flexbox and other types of CSS principles. This website also utilized react-router to route through the components on the page using Hash Links," +
+                "It is a single page responsive application that responds through media queries to hold responsivity",
+    tech: ['CSS', 'HTML', 'React.js'], 
     image: web,
     },
     {
     id: 2,
+    title: "Maker Pre-launch - Frontend Mentor",
+    github: "https://github.com/edgarcgriffinjr/Maker-Pre-Launch-FEM",
+    url: 'https://landing-fem-edgar.vercel.app/',
+    desc: "This is project built using React that is styled through CSS using gridbox. It is a pre-launch site for a company that was made up throught Frontend Mentor." + 
+                "This is a single page responsive application that adjusts CSS grid at various media queries to keep responsivity.",
+    tech: ['React', 'CSS'], 
+    image: maker,
+    },
+    {
+    id: 3,
     title: "Capstone Senior Project using CNN",
     github: 'null',
     url: "https://ak04526.wixsite.com/fall2020",
@@ -24,16 +35,7 @@ export const featuredData = [
     tech: ['Python', 'PyTorch', 'Jupyter Notebook', 'Ubuntu'],
     image: cnn,
     },
-    {
-    id: 3,
-    title: "College Database for the Computer Science Department",
-    github: "https://github.com/edgarcgriffinjr/CSDatabase",
-    url: 'null',
-    desc: "This is a project that was created by my team and I to ease the use of the class search tool provided by" +
-            " Georgia Southern. It was created to be specified to the Computer Science Department so it could be less" +
-            " confusing for students to find classes, teachers, and to add them.",
-    tech: ['Python', 'MySQL'], 
-    },
+    
 ]
 
 
